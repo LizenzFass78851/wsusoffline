a29ec12131673547fcf38867b73314b6cc8c4994
aker <6231347-wsusoffline@users.noreply.gitlab.com>
2020-07-19 00:32:34 +0200

    move internal version check

--- cmd/DownloadUpdates.cmd
+++ cmd/DownloadUpdates.cmd
@@ -44,6 +44,14 @@ if exist .\custom\InitializationHook.cmd (
   set ERR_LEVEL=
 )
 call :Log "Info: Starting WSUS Offline Update - Community Edition - download v. %WSUSOFFLINE_VERSION% for %1 %2"
+
+rem ** disable SDD, if local version != most recent version ***
+call CheckOUVersion.cmd /quiet
+if errorlevel 1 (
+  set SKIP_SDD=1
+  call :Log "Info: Disabled static and exclude definitions update due to version mismatch"
+)
+
 for %%i in (w62-x64 w63 w63-x64 w100 w100-x64 ofc o2k16) do (
   if /i "%1"=="%%i" (
     if /i "%2"=="glb" goto EvalParams
@@ -189,13 +197,6 @@ shift /3
 goto EvalParams
 
 :NoMoreParams
-rem ** disable SDD, if local version != most recent version ***
-call CheckOUVersion.cmd /quiet
-if errorlevel 1 (
-  set SKIP_SDD=1
-  call :Log "Info: Disabled static and exclude definitions update due to version mismatch"
-)
-
 echo %1 | %SystemRoot%\System32\find.exe /I "x64" >nul 2>&1
 if errorlevel 1 (set TARGET_ARCH=x86) else (set TARGET_ARCH=x64)
 if "%SKIP_TZ%"=="1" goto SkipTZ
