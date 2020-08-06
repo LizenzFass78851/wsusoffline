c97eac53d39a5b66ec488a1bc7bb75e937a3e5a6
aker <6231347-wsusoffline@users.noreply.gitlab.com>
2020-08-06 22:50:34 +0200

    - C++ 2019 Redistributable Runtime Libraries updated to v. 14.27.29016.0 (Thanks to "harry")

--- client/cmd/DoUpdate.cmd
+++ client/cmd/DoUpdate.cmd
@@ -10,7 +10,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=12.3 (b1)
+set WSUSOFFLINE_VERSION=12.3 (b2)
 title %~n0 %*
 echo Starting WSUS Offline Update - Community Edition - v. %WSUSOFFLINE_VERSION% at %TIME%...
 set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
