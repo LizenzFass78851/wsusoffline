a666d95b7344fd6f3b6e80cced871b545bc87bcf
aker@wsusoffline <6231347-wsusoffline@users.noreply.gitlab.com>
2021-02-15 00:56:01 +0100

    fix syntax error (Thanks to Gerby)

--- client/cmd/DoUpdate.cmd
+++ client/cmd/DoUpdate.cmd
@@ -30,7 +30,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=12.5 (b33)
+set WSUSOFFLINE_VERSION=12.5 (b34)
 title %~n0 %*
 echo Starting WSUS Offline Update - Community Edition - v. %WSUSOFFLINE_VERSION% at %TIME%...
 set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
