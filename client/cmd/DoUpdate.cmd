bda6b3eb2e1faed3d519cd2473565190b84adc24
aker@wsusoffline <6231347-wsusoffline@users.noreply.gitlab.com>
2021-03-15 22:44:18 +0100

    - remove static definitions for KB5001079 and KB5001078 - update XSL filter for w100 to exclude Windows 10 and newer - update ExcludeList for w100

--- client/cmd/DoUpdate.cmd
+++ client/cmd/DoUpdate.cmd
@@ -32,7 +32,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=11.9.8 (b42)
+set WSUSOFFLINE_VERSION=11.9.8 (b43)
 title %~n0 %*
 echo Starting WSUS Offline Update - Community Edition - v. %WSUSOFFLINE_VERSION% at %TIME%...
 set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
