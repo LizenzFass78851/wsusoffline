b45bb05f8585f3942046040688d8116d9e83558e
aker@wsusoffline <6231347-wsusoffline@users.noreply.gitlab.com>
2021-05-09 17:47:40 +0200

    - Fix: Windows 8.1 / Server 2012 R2 April 2014 and November 2014 rollup will be excluded from supersedance checks

--- cmd/DownloadUpdates.cmd
+++ cmd/DownloadUpdates.cmd
@@ -35,7 +35,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=12.5 (b64)
+set WSUSOFFLINE_VERSION=12.5 (b65)
 title %~n0 %1 %2 %3 %4 %5 %6 %7 %8 %9
 echo Starting WSUS Offline Update - Community Edition - download v. %WSUSOFFLINE_VERSION% for %1 %2...
 set DOWNLOAD_LOGFILE=..\log\download.log
@@ -1411,6 +1411,11 @@ for %%i in (w62 w63) do (
     for /F "tokens=1* delims=,;" %%k in (..\client\static\custom\%%j) do (
       echo %%k>>"%TEMP%\ExcludeList-superseded-exclude.txt"
     )
+  )
+)
+for %%i in (upd1 upd2) do (
+  for /F %%j in ('type ..\client\static\StaticUpdateIds-w63-%%i.txt ^| find /i "kb"') do (
+    echo windows8.1-%%j>>"%TEMP%\ExcludeList-superseded-exclude.txt"
   )
 )
 for %%i in ("%TEMP%\ExcludeList-superseded-exclude.txt") do if %%~zi==0 del %%i
