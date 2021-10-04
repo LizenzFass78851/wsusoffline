38f3959659b05ed41dda68b2393078b7f9e7fce5
aker@wsusoffline <6231347-wsusoffline@users.noreply.gitlab.com>
2021-10-04 18:25:17 +0200

    Fix: The calculation of superseded updates fails when running from a release configuration (Thanks to "rhardy613" and "rbronca")

--- client/cmd/DoUpdate.cmd
+++ client/cmd/DoUpdate.cmd
@@ -30,7 +30,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=12.6 (b37)
+set WSUSOFFLINE_VERSION=12.6 (b38)
 title %~n0 %*
 echo Starting WSUS Offline Update - Community Edition - v. %WSUSOFFLINE_VERSION% at %TIME%...
 set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
