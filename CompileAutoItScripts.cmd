558decb333a559002f8327fa2b48b15f32e356d4
aker@wsusoffline <6231347-wsusoffline@users.noreply.gitlab.com>
2022-03-24 12:56:23 +0100

    Fix: CompileAutoItScripts.cmd will now produce x86-executables on x64 systems again

--- CompileAutoItScripts.cmd
+++ CompileAutoItScripts.cmd
@@ -1,5 +1,6 @@
 @echo off
 rem *** Author: T. Wittrock, Kiel ***
+rem ***   - Community Edition -   ***
 
 verify other 2>nul
 setlocal enableextensions
@@ -10,7 +11,7 @@ if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set AUT2EXE_EXE=Aut2exe_x64.exe) else
 )
 
 echo Compiling AutoIt-Scripts...
-for %%i in (UpdateGenerator.au3 client\UpdateInstaller.au3) do "%~dps0bin\%AUT2EXE_EXE%" /in "%%i" /icon "%~dps0ico\okshield.ico" /comp 0 /nopack
+for %%i in (UpdateGenerator.au3 client\UpdateInstaller.au3) do "%~dps0bin\%AUT2EXE_EXE%" /in "%%i" /icon "%~dps0ico\okshield.ico" /x86 /comp 0 /nopack
 goto EoF
 
 :NoExtensions
--- client/cmd/DoUpdate.cmd
+++ client/cmd/DoUpdate.cmd
@@ -31,7 +31,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=11.9.12 (b24r2)
+set WSUSOFFLINE_VERSION=11.9.12 (b25)
 title %~n0 %*
 echo Starting WSUS Offline Update - Community Edition - v. %WSUSOFFLINE_VERSION% at %TIME%...
 set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
