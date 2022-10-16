2e1a400998a0670a3f63a0a6a0d1b3edad43ec5f
aker@wsusoffline <6231347-wsusoffline@users.noreply.gitlab.com>
2022-10-16 10:13:33 +0200

    - misc. improvements to InstallOSUpdate.cmd and InstallOfficeUpdate.com (Thanks to "aker")

--- client/cmd/DoUpdate.cmd
+++ client/cmd/DoUpdate.cmd
@@ -31,7 +31,7 @@ if "%DIRCMD%" NEQ "" set DIRCMD=
 
 cd /D "%~dp0"
 
-set WSUSOFFLINE_VERSION=12.7 (b65)
+set WSUSOFFLINE_VERSION=12.7 (b67)
 title %~n0 %*
 echo Starting WSUS Offline Update - Community Edition - v. %WSUSOFFLINE_VERSION% at %TIME%...
 set UPDATE_LOGFILE=%SystemRoot%\wsusofflineupdate.log
